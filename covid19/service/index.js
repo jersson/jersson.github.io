96074ad4d8c9c6e8e89e7dacf2eb16d4b7857793
Jersson Dongo <Jersson@gmail.com>
2020-03-21 16:48:37 -0500

    Add confirmed cases march21th

--- covid19/service/index.js
+++ covid19/service/index.js
@@ -3,27 +3,27 @@ exports.covidPeru = (req, res) => {
         region   : "lima",
         latitude : -12.0464,
         longitude: -77.0428, 
-        confirmed: 212
+        confirmed: 241
     },{
         region : "loreto",
         latitude : -3.74912,
         longitude: -73.25383, 
-        confirmed: 12
+        confirmed: 14
     },{
         region : "junin",
         latitude : -12.069156,
         longitude: -75.208648, 
-        confirmed: 9
+        confirmed: 10
     },{
         region : "lambayeque",
         latitude : -6.7011099,
         longitude: -79.9061127, 
-        confirmed: 6
+        confirmed: 8
     },{
         region : "callao",
         latitude : -12.0565901,
         longitude: -77.1181412, 
-        confirmed: 5
+        confirmed: 6
     },{
         region : "ancash",
         latitude : -9.1366700,
@@ -33,7 +33,7 @@ exports.covidPeru = (req, res) => {
         region : "arequipa",
         latitude : -16.3988900,
         longitude: -71.5350000, 
-        confirmed: 3
+        confirmed: 4
     },{
         region : "huanuco",
         latitude : -9.925791,
@@ -43,17 +43,17 @@ exports.covidPeru = (req, res) => {
         region : "la-libertad",
         latitude : -8.1159897,
         longitude: -79.0299835, 
-        confirmed: 2
+        confirmed: 5
     },{
         region : "piura",
         latitude : -5.1944900,
         longitude: -80.6328200, 
-        confirmed: 2 
+        confirmed: 18 
     },{
         region : "cusco",
         latitude : -13.516667,
         longitude: -71.978771, 
-        confirmed: 5
+        confirmed: 4
     },{
         region : "ica",
         latitude : -13.71029,
