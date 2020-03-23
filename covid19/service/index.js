6d155ed6e46c84658813805c4122a795dbe928e0
Jersson Dongo <Jersson@gmail.com>
2020-03-23 18:48:46 -0500

    Add confirmed label

--- covid19/service/index.js
+++ covid19/service/index.js
@@ -1,26 +1,31 @@
 exports.covidPeru = (req, res) => {
     let locations = [{
         region   : "lima",
+        title    : "Lima",
         latitude : -12.0464,
         longitude: -77.0428, 
         confirmed: 307
     },{
         region : "loreto",
+        title: "Loreto",
         latitude : -3.74912,
         longitude: -73.25383, 
         confirmed: 16
     },{
         region : "junin",
+        title: "Junin",
         latitude : -12.069156,
         longitude: -75.208648, 
         confirmed: 10
     },{
         region : "lambayeque",
+        title: "Lambayeque",
         latitude : -6.7011099,
         longitude: -79.9061127, 
         confirmed: 8
     },{
         region : "callao",
+        title: "Callao",
         latitude : -12.0565901,
         longitude: -77.1181412, 
         confirmed: 8
@@ -31,47 +36,56 @@ exports.covidPeru = (req, res) => {
         confirmed: 4
     },{
         region : "arequipa",
+        title: "Arequipa",
         latitude : -16.3988900,
         longitude: -71.5350000, 
         confirmed: 7
     },{
         region : "huanuco",
+        title: "Huánuco",
         latitude : -9.925791,
         longitude: -76.242772, 
         confirmed: 2
     },{
         region : "la-libertad",
+        title: "La Libertad",
         latitude : -8.1159897,
         longitude: -79.0299835, 
         confirmed: 4
     },{
         region : "piura",
+        title: "Piura",
         latitude : -5.1944900,
         longitude: -80.6328200, 
         confirmed: 19 
     },{
         region : "cusco",
+        title: "Cusco",
         latitude : -13.516667,
         longitude: -71.978771, 
         confirmed: 6
     },{
         region : "ica",
+        title: "Ica",
         latitude : -13.71029,
         longitude: -76.2053833, 
         confirmed: 2
     },{
         region : "madre-de-dios",
+        title: "Madre de Dios",
         latitude : -12.594215,
         longitude: -69.176401, 
         confirmed: 1
     },{
         region : "san-martin",
+        title: "San Martín",
         latitude : -6.034608,
         longitude: -76.974733, 
         confirmed: 1
     }];
 
     res.set('Access-Control-Allow-Origin', 'https://jersson.github.io')
+    // res.set('Access-Control-Allow-Origin', '*')
 
     res.send(locations);
 }
