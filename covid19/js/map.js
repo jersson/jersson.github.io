ba0356a37b5ef9c72e231549ff4d23485f5d40b5
Jersson Dongo <Jersson@gmail.com>
2020-03-31 21:46:37 -0500

    Refactor web architecture

---
+++ covid19/js/map.js
@@ -0,0 +1,54 @@
+function initMap() {
+  const maxI = 50, rad = 24, opac = .6;
+  const url = 'https://us-central1-virtual-bonito-170805.cloudfunctions.net/covidPeru';
+  document.getElementById('map').style.display = 'block';
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: {lat: -10.151093, lng: -75.311132},
+    mapTypeId: 'roadmap'
+  });
+
+  fetch(url).then(function(response) {
+    response.json().then(function(result) {
+
+      let locations = [];
+
+      result.forEach(location => {
+
+        let factor = 10 * location.confirmed;
+        let i = 0;
+
+        let image = './images/icons8-coronavirus-16.png';
+
+        while (i < factor) {
+          locations.push(new google.maps.LatLng(location.latitude, location.longitude));
+          
+          let contentString = `<div class="subject-2"><b>${location.title}</b><br/>Confirmados: <b>${location.confirmed}(${location.change})</b></div>`;
+          let infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+
+          let marker = new google.maps.Marker({
+            position: {lat: location.latitude, lng: location.longitude},
+            map: map,
+            icon: image
+          });
+
+          marker.addListener('click', function() {
+            infowindow.open(map, marker);
+          });
+
+          i++;
+        }
+      });
+
+      let heatmap = new google.maps.visualization.HeatmapLayer({
+        data: locations,
+        map: map,
+        maxIntensity: maxI,
+        radius: rad,
+        opacity: opac
+      });
+    });
+  });
+}
