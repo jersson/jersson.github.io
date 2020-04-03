fbee0b0db3e36e6c1bcbc7b1db1561b1aba81776
Jersson Dongo <Jersson@gmail.com>
2020-04-02 21:13:00 -0500

    Fix summary config typo

--- covid19/service/summary/index.js
+++ covid19/service/summary/index.js
@@ -6,7 +6,7 @@ exports.covidSummary = (req, res) =>{
                 {alias: 'Confirmados', reported: 1414, delta:'91+'},
                 {alias: 'Descartados', reported: 15104, delta:'840+'},
                 {alias: 'Recuperados', reported: 537, delta:'90+'},
-                {alias: 'Fallecidos', reported: 55, delta:'8='},
+                {alias: 'Fallecidos', reported: 55, delta:'8+'},
             ],
             source: {
                 link: 'https://twitter.com/Minsa_Peru/status/1245776225207009292',
