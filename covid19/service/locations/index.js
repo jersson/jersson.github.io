0b8da4674af313c9d843365b85aa8e8e9a66a937
Jersson Dongo <Jersson@gmail.com>
2020-04-07 00:24:48 -0500

    Update april6th info

--- covid19/service/locations/index.js
+++ covid19/service/locations/index.js
@@ -4,50 +4,50 @@ exports.covidLocations = (req, res) => {
         title    : "Lima",
         latitude : -12.0464,
         longitude: -77.0428, 
-        confirmed: 1639,
-        change: "382+"
+        confirmed: 1837,
+        change: "198+"
     },{
         region : "loreto",
         title: "Loreto",
         latitude : -3.74912,
         longitude: -73.25383, 
-        confirmed: 143, 
-        change: "37+"
+        confirmed: 171, 
+        change: "28+"
     },{
         region : "junin",
         title: "Junín",
         latitude : -12.069156,
         longitude: -75.208648, 
-        confirmed: 25,
-        change : "="
+        confirmed: 28,
+        change : "3+"
     },{
         region : "lambayeque",
         title: "Lambayeque",
         latitude : -6.7011099,
         longitude: -79.9061127, 
         confirmed: 100,
-        change: "45+"
+        change: "="
     },{
         region : "callao",
         title: "Callao",
         latitude : -12.0565901,
         longitude: -77.1181412, 
-        confirmed: 89,
-        change: "25+"
+        confirmed: 109,
+        change: "20+"
     },{
         region : "ancash",
         title : "Áncash",
         latitude : -9.1366700,
         longitude: -77.7602800, 
-        confirmed: 23,
-        change: "5+"
+        confirmed: 27,
+        change: "4+"
     },{
         region : "arequipa",
         title: "Arequipa",
         latitude : -16.3988900,
         longitude: -71.5350000, 
-        confirmed: 51,
-        change: "14+"
+        confirmed: 60,
+        change: "9+"
     },{
         region : "huanuco",
         title: "Huánuco",
@@ -60,29 +60,29 @@ exports.covidLocations = (req, res) => {
         title: "La Libertad",
         latitude : -8.1159897,
         longitude: -79.0299835, 
-        confirmed: 50,
-        change: "="
+        confirmed: 56,
+        change: "6+"
     },{
         region : "piura",
         title: "Piura",
         latitude : -5.1944900,
         longitude: -80.6328200, 
-        confirmed: 29, 
-        change: "2+"
+        confirmed: 30, 
+        change: "1+"
     },{
         region : "cusco",
         title: "Cusco",
         latitude : -13.516667,
         longitude: -71.978771, 
-        confirmed: 52,
-        change: "4+"
+        confirmed: 54,
+        change: "2+"
     },{
         region : "ica",
         title: "Ica",
         latitude : -13.71029,
         longitude: -76.2053833, 
-        confirmed: 14,
-        change: "5+"
+        confirmed: 18,
+        change: "3+"
     },{
         region : "madre-de-dios",
         title: "Madre de Dios",
@@ -103,7 +103,7 @@ exports.covidLocations = (req, res) => {
         latitude : -3.570698,
         longitude: -80.459641, 
         confirmed: 26,
-        change: "6+"
+        change: "="
     },{
         region : "cajamarca",
         title: "Cajamarca",
@@ -116,29 +116,29 @@ exports.covidLocations = (req, res) => {
         title: "Pasco",
         latitude : -10.683354,
         longitude: -76.256148, 
-        confirmed: 3,
-        change: "1+"
+        confirmed: 5,
+        change: "2+"
     },{
         region : "tacna",
         title: "Tacna",
         latitude : -18.013473,
         longitude: -70.250829, 
         confirmed: 9,
-        change: "6+"
+        change: "="
     },{
         region : "ayacucho",
         title: "Ayacucho",
         latitude : -13.160333,
         longitude: -74.225743, 
-        confirmed: 2,
+        confirmed: 5,
         change: "2+"
     },{
         region : "huancavelica",
         title: "Huancavelica",
         latitude : -12.787246,
         longitude: -74.973149, 
-        confirmed: 1,
-        change: "="
+        confirmed: 2,
+        change: "1+"
     },{
         region : "apurimac",
         title: "Apurimac",
@@ -152,7 +152,7 @@ exports.covidLocations = (req, res) => {
         latitude : -17.193771,
         longitude: -70.934572, 
         confirmed: 1,
-        change: "1+"
+        change: "="
     }];
 
     res.set('Access-Control-Allow-Origin', 'https://jersson.github.io')
--- covid19/service/summary/index.js
+++ covid19/service/summary/index.js
@@ -1,16 +1,16 @@
 exports.covidSummary = (req, res) =>{
     let summary = {
             show: true,
-            title : 'Situación al 5 de abril',
+            title : 'Situación al 6 de abril',
             cases: [
-                {alias: 'Confirmados', reported: '2,281', delta:'535+'},
-                {alias: 'Descartados', reported: '17,129', delta:'1,034+'},
-                {alias: 'Recuperados', reported: '989', delta:'75+'},
-                {alias: 'Fallecidos', reported: '83', delta:'10+'},
+                {alias: 'Pruebas realizadas', reported: '20,414', delta:'2,573+'},
+                {alias: 'Confirmados', reported: '2,561', delta:'280+'},
+                {alias: 'Recuperados', reported: '997', delta:'8+'},
+                {alias: 'Fallecidos', reported: '92', delta:'9+'},
             ],
             source: {
-                link: 'https://covid19.minsa.gob.pe/sala_situacional.asp', 
-                alias: 'MINSA'
+                link: '', 
+                alias: 'Presidente del Perú'
             }
         };
 
